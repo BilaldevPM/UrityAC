986f7a421eadc46e6dbb16e240be09cf82d372ef
Bilal <103137319+BilaldevPM@users.noreply.github.com>
2022-07-31 22:24:46 +0000

    Config Fix

--- src/Checks/Movement/Speed.php
+++ src/Checks/Movement/Speed.php
@@ -10,16 +10,13 @@ use pocketmine\player\GameMode;
 use pocketmine\Server;
 use pocketmine\player\Player;
 // custom
+use OnlyJaiden\UrityAC\Main;
 use OnlyJaiden\UrityAC\Alert;
 use OnlyJaiden\UrityAC\User;
 use OnlyJaiden\UrityAC\Player\UrityPlayer;
 
 class Speed implements Listener{
 
-    public function __construct() {
-    $this->MaxSpeed = 0.0;
-    $this->configspeed = Main::getInstance()->getConfig()->get("speed.max");
-}
     public function PlayerMove(PlayerMoveEvent $event) {
         $config = Main::getInstance()->getConfig();
         $player = $event->getPlayer();
