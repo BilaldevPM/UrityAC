53a1199638fb8c0a315b1c08cde18ce22520e307
Ihsan48 <101813808+Ihsan48@users.noreply.github.com>
2022-08-06 07:36:53 +0700

    Formatting

--- src/Main.php
+++ src/Main.php
@@ -4,47 +4,50 @@ declare(strict_types=1);
 
 namespace OnlyJaiden\UrityAC;
 
-use OnlyJaiden\UrityAC\User;
-use OnlyJaiden\UrityAC\Player\PlayerEvent;
-use OnlyJaiden\UrityAC\Checks\Loader;
-use pocketmine\utils\SingletonTrait;
-use pocketmine\player\Player;
-use pocketmine\event\Listener;
 use pocketmine\plugin\PluginBase;
+use pocketmine\utils\SingletonTrait;
+
+use pocketmine\player\Player;
+
+use pocketmine\event\Listener;
+
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 
-class Main extends PluginBase{
+use OnlyJaiden\UrityAC\User;
+use OnlyJaiden\UrityAC\Player\PlayerEvent;
+use OnlyJaiden\UrityAC\Checks\Loader;
+
+class Main extends PluginBase {
     use SingletonTrait;
     
-    public function onLoad(): void {
+    public function onLoad() : void {
         self::setInstance($this);
     }
     
-    public function onEnable(): void {
+    public function onEnable() : void {
         $loader = new Loader;
         $loader->LoadChecks();
         // Player Event Handler
         $this->registerEvents(new PlayerEvent());
     }
     
-    public function registerEvents(Listener $listener): void {
+    public function registerEvents(Listener $listener) : void {
         $this->getServer()->getPluginManager()->registerEvents($listener, $this);
     }
     
-    public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool{
-    switch($command->getName()){
-        case "alerts":
-            if($sender->hasPermission("UrityAC.alerts")){
-                $config = Main::getInstance()->getConfig();
-                $user = new User;
-                $user->checkAlert($sender);
-            } else {
-                $sender->sendMessage("You don't have permission to run this command!");
-            }
+    public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool {
+        switch($command->getName()){
+            case "alerts":
+                if($sender->hasPermission("UrityAC.alerts")){
+                    $config = Main::getInstance()->getConfig();
+                    $user = new User;
+                    $user->checkAlert($sender);
+                } else {
+                    $sender->sendMessage("You don't have permission to run this command!");
+                }
             return true;
-    }
+        }
       return true;
-}
-
+    }
 }
